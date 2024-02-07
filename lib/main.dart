0d2257da4408e6077b9f9303ef5d3e8e6237e873
Ryan Weald <ryan@weald.com>
2024-02-07 12:18:19 -0800

    Begin mapping channel method strings to flutter SDK actions
    Currently you can do basic CRUD on a wallet.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:rly_network_flutter_sdk/wallet_manager.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,20 +10,33 @@ class MyApp extends StatelessWidget {
   static const channel = MethodChannel('channels.rly.network/wallet_manager');
 
   Future<dynamic> channelMethodHandler(MethodCall methodCall) async {
-    switch (methodCall.method) {
-      case 'performSomeAction':
-        print('performSomeAction called');
-        return 5;
-      default:
-        throw PlatformException(
-            code: 'Unimplemented',
-            details:
-                'The method ${methodCall.method} is not implemented in WalletManager');
+    String action = methodCall.method;
+    if (action == "createWallet") {
+      print("Going to create a wallet");
+      await WalletManager.getInstance().createWallet();
+      return await WalletManager.getInstance().getPublicAddress();
     }
+    if (action == "getWalletAddress") {
+      print("calling getWalletAddress");
+      String? address = await WalletManager.getInstance().getPublicAddress();
+      return address ?? "no address";
+    }
+
+    if (action == "deleteWallet") {
+      print("calling deleteWallet");
+      await WalletManager.getInstance().permanentlyDeleteWallet();
+      return true;
+    }
+
+    throw PlatformException(
+        code: 'Unimplemented',
+        details:
+            'The method ${methodCall.method} is not implemented in WalletManager');
   }
 
   @override
   Widget build(BuildContext context) {
+    print("Setting up channel");
     channel.setMethodCallHandler(channelMethodHandler);
     return const MaterialApp();
   }
